f9542f9eb0ab41c9717a39beee97c6bae6e9f76a
Saravana <saravanankmk17@gmail.com>
2022-05-08 16:58:36 +0530

    Update script.js for receiving ideas and suggestions

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 var userMessage = 'Use Landscape Mode for better Experience!!!';
-
+var userMessageTwo = 'Help me improve this website by giving suggestions. You can use the contact me page at the bottom of the website';
 setTimeout(function(){
   alert(userMessage);
 }, 2000);
 
 setTimeout(function() {
-  alert('Give me some ideas to improve this website further friends');
-}, 2000);
+  alert(userMessageTwo);
+}, 3000);
