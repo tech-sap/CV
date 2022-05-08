f0b7fc0f37aef6e310ea6a2c964093d1aae28695
Saravana <saravanankmk17@gmail.com>
2022-05-08 16:48:52 +0530

    Update script.js to add a message

--- js/script.js
+++ js/script.js
@@ -2,5 +2,8 @@ var userMessage = 'Use Landscape Mode for better Experience!!!';
 
 setTimeout(function(){
   alert(userMessage);
-  window.orientation = 90;
+}, 2000);
+
+setTimeout(function() {
+  alert('Give me some ideas to improve this website further friends');
 }, 2000);
